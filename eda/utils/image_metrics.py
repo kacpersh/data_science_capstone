d16a290f90fa3b276303cac78c7110b5a4833cba
Lekshmi <les505@g.harvard.edu>
2021-10-11 00:11:12 -0700

    feat: Implemented helper function to calculate text area summaries per image

--- eda/utils/image_metrics.py
+++ eda/utils/image_metrics.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
 import numpy as np
+from nltk.tokenize import word_tokenize
 
 
 def get_image_entropy_and_size(img_path: str) -> tuple:
@@ -22,3 +23,32 @@ def get_bounding_box_area(x: np.ndarray, y: np.ndarray) -> float:
     :returns: A floating point number which represents the area of the quadrilateral.
     """
     return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+
+
+def get_text_area_summary(bounding_box_array: np.ndarray, num_of_words: int) -> tuple:
+    """Calculates the summary statistics of text area in an image.
+    :param np.ndarray bounding_box_array: An array of bounding box co-ordinates, one per word.
+    :param int num_of_words: An integer for the total number of words in an image.
+    :returns: A tuple containing the total text area, largest text area, smallest text area and
+        average text area per image.
+    """
+    bounding_box_array = bounding_box_array.T
+    areas = []
+
+    # For images with only one word, bounding box array has one less dimension.
+    # We need to rearrange the co-ordinates in the anti-clockwise direction for shoelace algorithm to work.
+    # For each word, calculate the bounding box area.
+    for i in range(num_of_words):
+        if num_of_words == 1:
+            x, y = (
+                bounding_box_array[[0, 3, 2, 1], 0],
+                bounding_box_array[[0, 3, 2, 1], 1],
+            )
+        else:
+            x, y = (
+                bounding_box_array[i][[0, 3, 2, 1], 0],
+                bounding_box_array[i][[0, 3, 2, 1], 1],
+            )
+        area = get_bounding_box_area(x, y)
+        areas.append(area)
+    return sum(areas), max(areas), min(areas), np.mean(areas)
