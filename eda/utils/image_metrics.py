d6e735bcbd1f99c4ae52f3c5dd0b197412414139
Lekshmi <les505@g.harvard.edu>
2021-10-10 23:56:55 -0700

    feat: Implemented shoelace algorithm to calculate polygon areas

--- eda/utils/image_metrics.py
+++ eda/utils/image_metrics.py
@@ -1,5 +1,7 @@
 from PIL import Image
 
+import numpy as np
+
 
 def get_image_entropy_and_size(img_path: str) -> tuple:
     """Calculates the complexity and size of any image.
@@ -11,3 +13,12 @@ def get_image_entropy_and_size(img_path: str) -> tuple:
         height = im.height
         width = im.width
     return entropy, height, width
+
+
+def get_bounding_box_area(x: np.ndarray, y: np.ndarray) -> float:
+    """Calculates the area of a quadrilateral using shoelace algorithm from x and y co-ordinates.
+    :param np.ndarray x: An array of ints representing the x co-ordinates of the quadrilateral
+    :param np.ndarray y: An array of ints representing the y co-ordinates of the quadrilateral
+    :returns: A floating point number which represents the area of the quadrilateral.
+    """
+    return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
